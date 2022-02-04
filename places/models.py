bc8c069946b810daa0d694494a43e2cadcec6a66
itcosplay <azure_dream@mail.ru>
2022-02-04 15:52:36 +0300

    Added  model for images

--- places/models.py
+++ places/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class Place(models.Model):
     title = models.CharField(
-        verbose_name='название',
-        max_length=200
+        max_length=200,
+        verbose_name='название'
+        
     )
     description_short = models.CharField(
-        verbose_name='краткое описание',
-        max_length=500
+        max_length=500,
+        verbose_name='краткое описание'
+        
     )
     description_long = models.TextField(
         verbose_name='полное описание'
@@ -18,6 +21,25 @@ class Place(models.Model):
     )
     lontitude = models.FloatField(
         verbose_name='lng'
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Image(models.Model):
+    title = models.ForeignKey(
+        'Place',
+        related_name='images',
+        on_delete=CASCADE,
+        verbose_name='название'
+
+    )
+    img = models.ImageField(
+        verbose_name='изображение'
+    )
+    order_number = models.IntegerField(
+        verbose_name='порядковый номер'
     )
 
     def __str__(self):
