69de3dbb9dd67f60c785e1f4a4f83645d8f685fa
itcosplay <azure_dream@mail.ru>
2022-02-04 13:04:30 +0300

    Added Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(
+        verbose_name='название',
+        max_length=200
+    )
+    description_short = models.CharField(
+        verbose_name='краткое описание',
+        max_length=500
+    )
+    description_long = models.TextField(
+        verbose_name='полное описание'
+    )
+    latitude = models.FloatField(
+        verbose_name='lat'
+    )
+    lontitude = models.FloatField(
+        verbose_name='lng'
+    )
+
+    def __str__(self):
+        return f'{self.title}'
